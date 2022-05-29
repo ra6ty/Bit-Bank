2c5b5b476b21dc20ef2c36bd5103a19f17cac1d7
Sviat <sviat.t123@gmail.com>
2022-05-29 17:06:31 +0300

    add footer and button to header for choose language

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import "./Main.scss"
 import Header from "../../component/Header/Header";
+import Footer from "../../component/Footer/Footer";
 
 const Main: React.FC = () => {
 
     return (
         <div className="Main">
             <Header/>
+            <main>
+
+            </main>
+            <Footer/>
         </div>
     );
 };
